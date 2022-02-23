2b9bee5f8930449656d486f96f4d3596a85aece5
Vignesh Kathirkmar <i51vignesh@gmail.com>
2022-02-23 11:08:11 +0530

    made date dynamic

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function renderTime()
+{
+    // date
+    var mydate = new Date();
+    var year = mydate.getYear();
+        if(year < 1000)
+        {
+            year+=1900
+        }
+    var day = mydate.getDay();
+    var month = mydate.getMonth();
+    var daym = mydate.getDate();
+    var dayarray = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday")
+    var montharray = new Array("January","Feburary","March","April","May","June","July","August","September","October","November","December")
+    //date end
+
+    var myClock = document.getElementById("clockDisplay")
+    myClock.textContent = "" +dayarray[day] + " "+ daym + " " +montharray[month]+ " "+year
+    // myClock.innerText = "" +dayarray[day] + " "+ daym + " " +montharray[month]+ " "+year
+    myClock.innerText = ""+daym+ " "+montharray[month]+ " "+year
+
+    setTimeout("renderTime()", 1000)
+}
+renderTime();
