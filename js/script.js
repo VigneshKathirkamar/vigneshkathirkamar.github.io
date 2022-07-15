4ab3cbe1461c705b8fbedeef9f75aa80f0584776
Vignesh Kathirkamar <59691368+VigneshKathirkamar@users.noreply.github.com>
2022-07-15 13:50:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-
+function welcomeStranger()
+{
+     alert("         Hey Stranger,\n         Thanks for the interest shown!!!")
+}
 
 function renderTime()
 {
     // date
-    alert("         Hey Stranger,\n         Thanks for the interest shown!!!")
     var mydate = new Date();
     var year = mydate.getYear();
         if(year < 1000)
